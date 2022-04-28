ec344dccb2c1143475c7282306a69184c8f03ed5
Mike Thomas <mikethomas84@gmail.com>
2022-04-28 15:46:18 +0100

    merged with correct line-endings

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,17 +1,18 @@
-import { MongoClient } from "mongodb"
-import { Database } from "../lib/types"
+import { MongoClient } from "mongodb";
+import { Database } from "../lib/types";
 
-const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/main?retryWrites=true&w=majority`
+const url = `mongodb+srv://${process.env.DB_USER}:${
+  process.env.DB_USER_PASSWORD
+}@${process.env.DB_CLUSTER}.mongodb.net`;
 
 export const connectDatabase = async (): Promise<Database> => {
+  const client = await MongoClient.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  const db = client.db("main");
 
-    const client = new MongoClient(url);
-    const db = client.db('main')
-
-    await client.connect()
-
-    return {
-        listings: db.collection("test_listings")
-    }
-}
-
+  return {
+    listings: db.collection("test_listings")
+  };
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,26 @@
-import express, { Application } from "express"
-import { ApolloServer } from "apollo-server-express"
-import { connectDatabase } from "./database"
-import { typeDefs, resolvers } from "./graphql"
+require("dotenv").config();
+
+import express, { Application } from "express";
+import { ApolloServer } from "apollo-server-express";
+import { connectDatabase } from "./database";
+import { typeDefs, resolvers } from "./graphql";
 
 const mount = async (app: Application) => {
-    const db = await connectDatabase()
+  const db = await connectDatabase();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: () => ({ db })
+  });
 
-    const server = new ApolloServer({ typeDefs, resolvers, context: () => ({ db }) })
-    server.applyMiddleware({ app, path: "/api" })
-    app.listen(process.env.PORT)
+  server.applyMiddleware({ app, path: "/api" });
+  app.listen(process.env.PORT);
 
-    const listings = await db.listings.find({}).toArray()
-    console.log(listings)
-}
+  console.log(`[app] : http://localhost:${process.env.PORT}`);
+};
 
-mount(express())
+mount(express());
+
+// Note: You will need to introduce a .env file at the root of the project
+// that has the PORT, DB_USER, DB_USER_PASSWORD, and DB_CLUSTER environment variables defined.
+// Otherwise, the server will not be able to start and/or connect to the database
