7b3407799b3af6c7eada21a935a0d233ebdfd3da
Mike Thomas <mikethomas84@gmail.com>
2022-04-29 13:20:21 +0100

    database file cleaned up

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,15 +1,10 @@
 import { MongoClient } from "mongodb";
 import { Database } from "../lib/types";
 
-const url = `mongodb+srv://${process.env.DB_USER}:${
-  process.env.DB_USER_PASSWORD
-}@${process.env.DB_CLUSTER}.mongodb.net`;
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net`;
 
 export const connectDatabase = async (): Promise<Database> => {
-  const client = await MongoClient.connect(url, {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true
-  });
+  const client = await MongoClient.connect(url);
   const db = client.db("main");
 
   return {
