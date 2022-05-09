89f7732aed67c07f5a6ea07f948db78e6f5c15bb
Mike Thomas <mikethomas84@gmail.com>
2022-05-09 20:23:29 +0100

    manual hooks replaced by apollo

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
 import { Listings } from './sections';
+
+const client = new ApolloClient({ 
+  uri: '/api', 
+  cache: new InMemoryCache()
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Listings title="Listings"  />
+    <ApolloProvider client={client}>
+     <Listings title="Listings"  />
+    </ApolloProvider>
   </React.StrictMode>
 );
